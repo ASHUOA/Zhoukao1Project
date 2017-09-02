710014774948ef84edabdee5d3ae8437437e1981
fanyishuo <1822176285@qq.com>
2017-09-02 11:32:02 +0800

    kkio

--- app/src/main/java/com/example/fanyishuo/zhoukao1/MainActivity.java
+++ app/src/main/java/com/example/fanyishuo/zhoukao1/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.fanyishuo.zhoukao1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyCustom mycustom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mycustom = (MyCustom) findViewById(R.id.mycustom);
+        mycustom.setButton1ClickListener(new MyCustom.OnButton1ClickListener() {
+            @Override
+            public void OnButton1Listener(View button1) {
+                Toast.makeText(MainActivity.this,"点击了返回", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mycustom.setButton2ClickListener(new MyCustom.OnButton2ClickListener() {
+            @Override
+            public void OnButton2Listener(View button2) {
+                Toast.makeText(MainActivity.this,"点击了更多", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
